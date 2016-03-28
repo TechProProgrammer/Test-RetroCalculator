8eae6c99c25c0cc0e775ce25fbcf1c0cc5086142
TechProProgrammers <info@mytechproshop.com>
2016-03-27 23:52:11 -0400

    Initial Commit

---
+++ RetroCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by TechPro Solutions on 3/27/16.
+//  Copyright © 2016 TechPro Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
